0472583c65af43adf220674f5c87f3d0241bc09a
keiadachi <2013edcckeiadachi@gmail.com>
2021-06-20 05:41:42 +0000

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :books
   attachment :profile_image, destroy: false
 
-  validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
+  validates :name, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50}
 
 end
