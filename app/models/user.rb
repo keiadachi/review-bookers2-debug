4850b5e9fdcf030ddbbe1db93b2f183fb5b818e1
keiadachi <2013edcckeiadachi@gmail.com>
2021-06-29 05:41:33 +0000

    user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,5 +17,17 @@ class User < ApplicationRecord
   has_many :followers, through: :reverse_of_relationships, source: :follower
   # 与フォロー関係を通じて参照→自分がフォローしている人
   has_many :followings, through: :relationships, source: :followed
+  
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 
 end
