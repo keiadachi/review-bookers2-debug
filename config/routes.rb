5c4536085562a2d20bd49bb0cbef0a86b4e92b35
keiadachi <2013edcckeiadachi@gmail.com>
2021-06-30 05:25:12 +0000

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
 
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end  
+  
   resources :books
 
 
